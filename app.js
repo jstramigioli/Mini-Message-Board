68117030fc1aca11af24aa0498c6e6d3ee6c7a4f
jstramigioli <julianstramigioli@gmail.com>
2025-05-27 21:55:57 -0300

    now listening to process.env port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Mini Message board - listening on port ${PORT}!`);
 });

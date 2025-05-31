f79a410680dad5918da96b1afd31d7820b9fde47
jstramigioli <julianstramigioli@gmail.com>
2025-05-30 21:02:22 -0300

    now using render database url

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,8 @@ require('dotenv').config();
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false, 
+    },
 });

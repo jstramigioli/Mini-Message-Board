5b0b37dd887f52dc36ededbaeb547fe00e9b6405
jstramigioli <julianstramigioli@gmail.com>
2025-05-30 21:23:38 -0300

    corrected bug, now populatedb connects with DATABASE_URL

--- db/populatedb.js
+++ db/populatedb.js
@@ -21,11 +21,8 @@ VALUES
 async function populate() {
   console.log("seeding...");
   const client = new Client({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
   });
   try {
     await client.connect();
